6c28b19dec5d95b899d6dc6769d24f332ecb636b
ericmay33 <ericbrianmay@optonline.net>
2025-11-17 17:44:21 -0500

    Fingerspelling Fallback

--- src/io/db_io.py
+++ src/io/db_io.py
@@ -2,6 +2,36 @@ from src.database.db_connection import DatabaseConnection
 from src.database.db_functions import get_sign_by_token
 import time
 
+# will fill with motion scripts for letters when hardware for hand supports signs
+FINGERSPELL_CACHE = {
+    "A": None,
+    "B": None,
+    "C": None,
+    "D": None,
+    "E": None,
+    "F": None,
+    "G": None,
+    "H": None,
+    "I": None,
+    "J": None,
+    "K": None,
+    "L": None,
+    "M": None,
+    "N": None,
+    "O": None,
+    "P": None,
+    "Q": None,
+    "R": None,
+    "S": None,
+    "T": None,
+    "U": None,
+    "V": None,
+    "W": None,
+    "X": None,
+    "Y": None,
+    "Z": None,
+}
+
 def run_database(file_io):
     DatabaseConnection.initialize()
     print("[DB_IO] Started database I/O handler loop.")
@@ -13,14 +43,33 @@ def run_database(file_io):
         # Process all tokens in the queue
         while not file_io.asl_token_queue.empty():
             token = file_io.pop_asl_token()
+
+            # Try to retreive sign data from database
             sign_data = get_sign_by_token(token)
+
             if sign_data:
                 file_io.push_motion_script(sign_data)
                 print(f"[DB_IO] Retrieved sign for token: {token}")
-            else:
-                # LOG WORDS NOT FOUND IN DATABASE FOR DEBUGGING
-                print(f"[DB_IO] Token '{token}' not found in database.")
-        
+                continue
+
+            # FALLBACK -> Fingerspelling
+            print(f"[DB_IO] Token '{token}' not in DB. Falling back to fingerspelling.")
+
+            for letter in token:
+                if not letter.isalpha():
+                    continue
+
+                letter = letter.upper()
+
+                # Check if the letter exists in cache
+                letter_sign = FINGERSPELL_CACHE.get(letter)
+
+                if letter_sign:
+                    file_io.push_motion_script(letter_sign)
+                    print(f"[DB_IO] Added fingerspelling letter: {letter}")
+                else:
+                    print(f"[DB_IO] WARNING: No fingerspelling JSON for '{letter}'")
+
         # Reset event after processing all tokens
         file_io.asl_new_signal.clear()
 
